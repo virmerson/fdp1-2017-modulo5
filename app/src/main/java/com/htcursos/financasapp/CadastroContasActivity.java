613e24e201e2d936c3285ed36eb5389de46dec7b
virmerson <virmerson@hightechcursos.com.br>
2017-10-21 17:10:13 -0300

    Intent, Butter Knife, transferindo dados...

--- app/src/main/java/com/htcursos/financasapp/CadastroContasActivity.java
+++ app/src/main/java/com/htcursos/financasapp/CadastroContasActivity.java
@@ -6,11 +6,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class CadastroContasActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.et_campo_conta)
+    EditText etConta;
+
+
+    @BindView(R.id.et_campo_valor)
+    EditText etValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +33,17 @@ public class CadastroContasActivity extends AppCompatActivity {
     @OnClick(R.id.btn_cadastrar)
     public void aoClicarCadastro(View view){
 
+
+        Bundle bundle = new Bundle();
+        bundle.putString("conta", etConta.getText().toString());
+        bundle.putString("valor", etValor.getText().toString());
+
+
         Intent irParaListaContas = new Intent(this, ListaContasActivity.class);
+        irParaListaContas.putExtras(bundle);
         startActivity(irParaListaContas);
+
+
 
     }
 }
--- app/src/main/java/com/htcursos/financasapp/ListaContasActivity.java
+++ app/src/main/java/com/htcursos/financasapp/ListaContasActivity.java
@@ -2,12 +2,35 @@ package com.htcursos.financasapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ListaContasActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_texto_conta)
+    TextView tvConta;
+
+    @BindView(R.id.tv_valor_conta)
+    TextView tvValor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_contas);
+
+        ButterKnife.bind(this);
+
+
+        Bundle bundle = getIntent().getExtras();
+        String conta = bundle.getString("conta");
+        String valor = bundle.getString("valor");
+
+
+        tvConta.setText(conta);
+        tvValor.setText(valor);
+
+
     }
 }
